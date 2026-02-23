356bb83c0eb06b77c0017ea5099183cd5859570a
Dark-Known <darkKnown029@gmail.com>
2026-02-23 17:27:48 +0530

    field validator in progress

--- Design-Pattern-common/src/main/java/Service/JsonValidator.java
+++ Design-Pattern-common/src/main/java/Service/JsonValidator.java
@@ -16,5 +16,29 @@ public class JsonValidator {
         {
             throw new IllegalArgumentException("Invalid Json Object");
         }
+        fieldValidator("Transport Mode", jsonobj.getTransportMode());
+        fieldValidator("Cargo Name", jsonobj.getCargo());
+        fieldValidator("User Name", jsonobj.getUserName());
+        fieldValidator("User Address", jsonobj.getUserAddress());
+        fieldValidator("Source Address", jsonobj.getSourceAddress());
+        fieldValidator("Destination Address", jsonobj.getDestAddress());
+        fieldValidator("Distance Information", jsonobj.getDistance());
+    }
+
+    private void fieldValidator(String fieldName, String fieldValue)
+    {
+        if(fieldValue== null || fieldValue.trim().isEmpty())
+        {
+            throw new IllegalArgumentException("Invalid "+fieldName);
+        }
+    }
+
+    private void fieldValidator(String fieldName , int fieldValue)
+    {
+        if(fieldValue<0)
+        {
+            throw new IllegalArgumentException("Invalid "+fieldName);
+        }
+
     }
 }
