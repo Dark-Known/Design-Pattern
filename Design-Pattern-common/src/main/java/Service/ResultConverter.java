fce739f9fc4eb54389ec1cb839e20f9c0823f8d4
Dark-Known <darkKnown029@gmail.com>
2026-02-26 16:03:58 +0530

    add methods to extract message and unit test simultaneously

--- Design-Pattern-common/src/main/java/Service/ResultConverter.java
+++ Design-Pattern-common/src/main/java/Service/ResultConverter.java
@@ -1,15 +1,34 @@
 package Service;
 
-import Utils.ValidationResult;
-
+import Utils.ErrorList;
+import Utils.ErrorType;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
 
 public class ResultConverter {
 
-//    public List<String> getListOfMessages()
-//    {
-//
-//    }
+    public List<String> getFailureMessage(ErrorList errorList)
+    {
+        return extractMessages(errorList,ErrorType.FAILURE);
+    }
+
+
+    public List<String> extractMessages(ErrorList errorList, ErrorType resultType)
+    {
+
+        List<String> MessageList = new ArrayList<>();
+        errorList.getResultList().forEach(
+                (validationResult)->{
+                    if(validationResult.getType()==resultType)
+                    {
+                        MessageList.add(validationResult.getMessage());
+                    }
+                }
+        );
+        return Collections.unmodifiableList(MessageList);
+
+
+    }
 }
+
---
+++ Design-Pattern-common/src/test/java/TestForService/TestForResultConverter.java
@@ -0,0 +1,62 @@
+package TestForService;
+
+import Service.ResultConverter;
+import Utils.ErrorList;
+import Utils.ErrorType;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class TestForResultConverter {
+
+    ErrorList errorList;
+    ResultConverter resultConverter;
+
+    @BeforeEach
+    void setUp()
+    {
+        errorList= ErrorList.empty();
+        errorList.withSuccess("Transaction Success for OP1");
+        errorList.withSuccess("Transaction Success for OP2");
+        errorList.withFailure("Transaction Failure for OP1");
+        errorList.withFailure("Transaction Failure for OP2");
+        errorList.withWarning("Transaction Successful with Warning for OP1");
+        errorList.withWarning("Transaction Successful with Warning for OP2");
+        resultConverter= new ResultConverter();
+    }
+
+
+    @Test
+    @DisplayName("Should return list of Messages based on error type")
+    void ShouldExtractMessageBasedOnType()
+    {
+        List<String> actualFailureList=  resultConverter.extractMessages(errorList, ErrorType.FAILURE);
+        List<String> expectedFailureList= List.of(
+                "Transaction Failure for OP1",
+                "Transaction Failure for OP2"
+
+        );
+
+        Assertions.assertEquals(2,actualFailureList.size());
+        Assertions.assertEquals(expectedFailureList.get(0),actualFailureList.get(0));
+        Assertions.assertEquals(expectedFailureList.get(1),actualFailureList.get(1));
+
+    }
+
+    @Test
+    @DisplayName("List extracted Should not be modifiable")
+    void ShouldThrowUnsupportedException()
+    {
+        List<String> actualFailureList=  resultConverter.extractMessages(errorList, ErrorType.FAILURE);
+
+        Assertions.assertThrows(UnsupportedOperationException.class,
+                ()->{
+                    actualFailureList.add("Test");
+                });
+
+    }
+}
