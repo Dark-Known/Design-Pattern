f88114d409fd53da32d8292e0ccc9e4700557648
Dark-Known <darkKnown029@gmail.com>
2026-02-27 15:47:03 +0530

    add

--- Design-Pattern-common/src/main/java/Service/IFieldValidator.java
+++ Design-Pattern-common/src/main/java/Service/IFieldValidator.java
@@ -6,7 +6,7 @@ public interface IFieldValidator {
    <T> ErrorList validateFormat(String fieldName,T fieldValue);
     <T> ErrorList validateValue(T fieldValue);
     <T> ErrorList isValid(String fieldName,T fieldValue);
-
+        void test();
 
 
 }
